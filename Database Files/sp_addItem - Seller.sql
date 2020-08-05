e16bf7a15c4bd036f1a281a5c8530ff423b5fa86
aditya1962 <abeysingheaditya4@gmail.com>
2020-08-05 22:14:41 +0530

    Added stored procedure for adding items in seller view

---
+++ Database Files/sp_addItem - Seller.sql
@@ -0,0 +1,57 @@
+use BuyGrandSeller;
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Aditya Abeysinghe
+-- Create date: 2020/08/05
+-- Description:	Stored procedure for adding an item in seller view 
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[sp_addItem] 
+	@description varchar(200),
+	@name varchar(50),
+	@price numeric(5,2),
+	@imagePath varchar(100),
+	@quantityAvailable int,
+	@categoryName varchar(15),
+	@subcategoryName varchar(15)
+AS
+BEGIN
+
+	SET XACT_ABORT, NOCOUNT ON;
+	DECLARE @transactionCount int;
+	SET @transactionCount = @@TRANCOUNT;
+	BEGIN TRY
+		IF @transactionCount=0
+			BEGIN TRANSACTION;
+		ELSE
+			SAVE TRANSACTION sp_addItem
+
+		DECLARE @itemID int, @categoryID int;
+		SELECT @itemID = (select top(1) itemID from dbo.item order by itemID desc);
+		SET @itemID = @itemID + 1;
+		SELECT @categoryID = (select categoryID from dbo.itemCategory where category=@categoryName and subcategory=@subcategoryName);
+
+
+		insert into dbo.item(itemID,description,name,price,imagePath,quantityAvailable,categoryID)
+					values(@itemID,@description,@name,@price,@imagePath,@quantityAvailable,@categoryID);
+
+		IF @transactionCount=0
+			COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		DECLARE @errorNo int, @message varchar(max), @xstate int
+		SELECT @errorNo = ERROR_NUMBER(), @message = ERROR_MESSAGE(), @xstate = XACT_STATE();
+		IF @transactionCount = 0
+			ROLLBACK TRANSACTION;
+		ELSE 
+			IF @xstate = 1
+				ROLLBACK TRANSACTION sp_addItem;
+			ELSE
+				ROLLBACK TRANSACTION;
+	END CATCH
+    
+END
+GO
