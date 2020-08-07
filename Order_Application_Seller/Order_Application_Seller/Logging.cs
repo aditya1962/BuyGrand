ceb85c10674a27dc4864543265619adc826a54e2
aditya1962 <abeysingheaditya4@gmail.com>
2020-08-07 17:11:46 +0530

    Added logging to seller view

---
+++ Order_Application_Seller/Order_Application_Seller/Logging.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Order_Application_Seller
+{
+    public class Logging
+    {
+        public static void WriteLog(Exception ex, string type, string message)
+        {
+            string file = "Logging-" + DateTime.Now.ToString("YYYY-MM-DD") + ".txt";
+            string destination = AppDomain.CurrentDomain.BaseDirectory + "\\Logging\\";
+            string path = destination + file;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    string logMessage = "";
+                    if (ex == null)
+                    {
+                        logMessage = DateTime.Now.ToString("HH:MM:SS") + "| " + type + " | " + message;
+                    }
+                    else
+                    {
+                        logMessage = DateTime.Now.ToString("HH:MM:SS") + "| " +
+                            ex + " | " + type + " | " + message;
+                    }
+                    writer.WriteLine(logMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
