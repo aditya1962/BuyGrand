f01e5eceea9babcc3a492c147105efa93814a794
aditya1962 <abeysingheaditya4@gmail.com>
2020-12-05 19:53:52 +0530

    Added Dashboard web service in ServiceApplicationBuyGrandSeller

---
+++ Order_Application_Seller/ServiceApplicationBuyGrandSeller/Dashboard.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Dashboard.asmx.cs" Class="ServiceApplicationBuyGrandSeller.Dashboard" %>
---
+++ Order_Application_Seller/ServiceApplicationBuyGrandSeller/Dashboard.asmx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServiceApplicationBuyGrandSeller
+{
+    [WebService(Namespace = "http://localhost/ServiceBuyGrandSeller/Dashboard.asmx/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class Dashboard : System.Web.Services.WebService
+    {
+        string rrConnectionString = ConfigurationManager.ConnectionStrings["SellerReadReplicaConnectionString"].ConnectionString;
+
+        [WebMethod]
+        public DataTable GetDashboard()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(rrConnectionString))
+                {
+                    connection.Open();
+                    DataTable table = new DataTable("DashboardData");
+                    SqlDataAdapter adapter = new SqlDataAdapter("sp_getDashboardData", connection);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    adapter.Fill(table);
+                    return table;
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.WriteLog(e, "Error", e.Message);
+                return null;
+            }
+        }
+
+        public DataTable GetChart()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(rrConnectionString))
+                {
+                    connection.Open();
+                    DataTable table = new DataTable("ChartData");
+                    SqlDataAdapter adapter = new SqlDataAdapter("sp_getChartData", connection);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    adapter.Fill(table);
+                    return table;
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.WriteLog(e, "Error", e.Message);
+                return null;
+            }
+        }
+
+
+    }
+}
