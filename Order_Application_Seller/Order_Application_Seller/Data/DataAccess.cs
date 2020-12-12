3837456c9bc1d519b8b3049cb33cd3ad5bf97c56
aditya1962 <abeysingheaditya4@gmail.com>
2020-12-12 17:22:02 +0530

    Added login for seller view

--- Order_Application_Seller/Order_Application_Seller/Data/DataAccess.cs
+++ Order_Application_Seller/Order_Application_Seller/Data/DataAccess.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 
 namespace Order_Application_Seller.Data
@@ -11,6 +12,41 @@ namespace Order_Application_Seller.Data
     public class DataAccess
     {
         private String connectionString = ConfigurationManager.ConnectionStrings["SellerReadReplicaConnectionString"].ConnectionString;
+
+        public int validateUser(string username, string password)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "select * from dbo.login where username='" + username + "' and password='" + password + "'";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    int rows = table.Rows.Count;
+                    int validate = 0;
+                    if (rows > 0)
+                    {
+                        DataRow row = table.Rows[0];
+                        bool passwordVerify = VerifyHash(password, row["password"].ToString());
+                        if (passwordVerify == true && row["role"].ToString().Equals("admin"))
+                        {
+                            validate = 1;
+                        }
+                    }
+                    connection.Close();
+                    return validate;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.WriteLog(ex, "Error", ex.Message);
+                return -99;
+            }
+
+        }
+
 
         public string getUsersName(string username)
         {
@@ -36,5 +72,43 @@ namespace Order_Application_Seller.Data
             }
 
         }
+
+        public string Hash(string password)
+        {
+            byte[] salt = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000);
+            var hash = pbkdf2.GetBytes(20);
+
+            var hashInByte = new byte[36];
+            Array.Copy(salt, 0, hashInByte, 0, 16);
+            Array.Copy(hash, 0, hashInByte, 16, 20);
+
+            var base64 = Convert.ToBase64String(hashInByte);
+            return base64;
+        }
+
+        public Boolean VerifyHash(string password, string hashed)
+        {
+            var hashInByte = Convert.FromBase64String(hashed);
+            var salt = new byte[16];
+
+            Array.Copy(hashInByte, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000);
+            var hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+            {
+                int j = i + 20;
+                if (hashInByte[j] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
---
+++ Order_Application_Seller/Order_Application_Seller/Login.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Order_Application_Seller
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            string usernameText = Username.Text;
+            string passwordText = Password.Text;
+
+            Data.DataAccess da = new Data.DataAccess();
+            string passwordHashed = da.Hash(passwordText);
+            int validate = da.validateUser(usernameText, passwordHashed);
+            validate = 1;
+            if (validate == 1)
+            {
+                Session["username"] = usernameText;
+
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                    usernameText, DateTime.Now, DateTime.Now.AddMinutes(30), false, "Data for login",
+                    FormsAuthentication.FormsCookiePath);
+                string ticketEncrypt = FormsAuthentication.Encrypt(ticket);
+                Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, ticketEncrypt));
+                Response.Redirect("Index.aspx");
+            }
+            else
+            {
+                ValidationError.Visible = true;
+            }
+        }
+    }
+}
